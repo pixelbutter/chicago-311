8344853b8158df61dd983db7bc1ce36c5b6dcbae
pixelbutter <pixel.butter.tech@gmail.com>
2017-11-15 22:15:18 -0600

    new request contact info ui base

--- app/src/main/java/com/chicago311/create/contact/NewRequestContactFragment.kt
+++ app/src/main/java/com/chicago311/create/contact/NewRequestContactFragment.kt
@@ -1,17 +1,34 @@
 package com.chicago311.create.contact
 
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.chicago311.R
 import com.chicago311.create.BaseStepperFragment
 import com.chicago311.create.NewRequestViewModel
+import com.stepstone.stepper.StepperLayout
 import com.stepstone.stepper.VerificationError
 
 class NewRequestContactFragment : BaseStepperFragment() {
+
+    private lateinit var viewModel: NewRequestContactViewModel
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_new_request_contact_info, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(NewRequestContactViewModel::class.java)
+    }
+
+    override fun onCompleteClicked(callback: StepperLayout.OnCompleteClickedCallback?) {
+        super.onCompleteClicked(callback)
+        // todo
+        Toast.makeText(context.applicationContext, "Not implemented yet!", Toast.LENGTH_SHORT).show()
     }
 
     override fun saveStepData(parentViewModel: NewRequestViewModel) {
---
+++ app/src/main/java/com/chicago311/create/contact/NewRequestContactViewModel.kt
@@ -0,0 +1,7 @@
+package com.chicago311.create.contact
+
+import android.arch.lifecycle.ViewModel
+
+class NewRequestContactViewModel : ViewModel() {
+
+}
