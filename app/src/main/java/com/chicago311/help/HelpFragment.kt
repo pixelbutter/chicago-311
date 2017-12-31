ad965959d2120a08220f6c88238dfadb1dd0dd1d
pixelbutter <pixel.butter.tech@gmail.com>
2017-12-31 03:33:27 -0600

    help tab

--- app/src/main/java/com/chicago311/help/HelpFragment.kt
+++ app/src/main/java/com/chicago311/help/HelpFragment.kt
@@ -3,10 +3,15 @@ package com.chicago311.help
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.text.Html
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.chicago311.R
+import kotlinx.android.synthetic.main.fragment_help.*
+
 
 class HelpFragment : Fragment() {
 
@@ -18,6 +23,19 @@ class HelpFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.title_help)
+        val linkMovementMethod = LinkMovementMethod.getInstance()
+        additionalInfoFaq.movementMethod = linkMovementMethod
+        additionalInfoFullSite.movementMethod = linkMovementMethod
+        callWhenValue.text = fromHtml(getString(R.string.call_when_value))
+    }
+
+    @SuppressWarnings("deprecated")
+    private fun fromHtml(htmlString: String): Spanned {
+        return if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            Html.fromHtml(htmlString, Html.FROM_HTML_MODE_LEGACY)
+        } else {
+            Html.fromHtml(htmlString)
+        }
     }
 
     companion object {
